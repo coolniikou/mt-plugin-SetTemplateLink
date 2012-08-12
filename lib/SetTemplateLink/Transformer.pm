29eaa96ea01f8ab163cb73ccadd5de4e9b399b24
coolniikou <cool.ni.ikou@gmail.com>
2012-08-12 12:29:46 +0900

    retouch

--- lib/SetTemplateLink/Transformer.pm
+++ lib/SetTemplateLink/Transformer.pm
@@ -6,13 +6,13 @@ use File::Spec;
 sub hdlr_template_param_edit_template {
     my ( $cb, $app, $param, $tmpl ) = @_;
 
+    my $q           = $app->param;
+    my $template_id = $q->param('id');
     my $blog_id     = $q->param('blog_id');
     my $plugin  = MT->component('SetTemplateLink');
     my $config_path =
       $plugin->get_config_value( 'set_template_path', "blog:$blog_id" );
 
-    my $q           = $app->param;
-    my $template_id = $q->param('id');
     my $template    = MT::Template->load({ id => $template_id, blog_id => $blog_id });
     my $linked_file =
         $template->linked_file()
