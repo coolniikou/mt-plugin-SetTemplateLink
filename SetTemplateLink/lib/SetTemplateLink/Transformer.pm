61dd2bceeeaf3c0e3865cbc375b4ea2e95e93a7b
coolniikou <cool.ni.ikou@gmail.com>
2012-10-16 01:54:55 +0900

    updated

--- SetTemplateLink/lib/SetTemplateLink/Transformer.pm
+++ SetTemplateLink/lib/SetTemplateLink/Transformer.pm
@@ -2,18 +2,40 @@ package SetTemplateLink::Transformer;
 
 use strict;
 use File::Spec;
+use Data::Dumper;
+
+sub hdlr_template_source_edit_template {
+		my ($cb, $app, $tmpl_ref) = @_;
+		my $jquery_old = <<EOF; 
+    jQuery('button.save, button.publish').click(function() {
+        syncEditor();
+        jQuery('form#template-listing-form > input[name=__mode]').val('save');
+    });
+EOF
+		$jquery_old = quotemeta($jquery_old);
+		$jquery_old =~ s!(\\ )+!\\s+!g;
+		my $jquery_new = <<EOF;
+    jQuery('button.save, button.publish').click(function() {
+        syncEditor();
+        jQuery('form#template-listing-form > input[name=__mode]').val('save');
+    });
+    jQuery('input#linked_file').val( jQuery('input#linked_file').val().replace('new-ping', jQuery('input#title').val()));
+		jQuery('button.save').focus();
+EOF
+		$$tmpl_ref =~ s!$jquery_old!$jquery_new!;
+}
 
 sub hdlr_template_param_edit_template {
     my ( $cb, $app, $param, $tmpl ) = @_;
 
-    my $q           = $app->param;
-    my $template_id = $q->param('id');
-    my $blog_id     = $q->param('blog_id');
+    my $blog_id = $app->blog->id;
     my $plugin  = MT->component('SetTemplateLink');
     my $config_path =
       $plugin->get_config_value( 'set_template_path', "blog:$blog_id" );
-
-    my $template    = MT::Template->load({ id => $template_id, blog_id => $blog_id });
+    my $q           = $app->param;
+    my $blog_id     = $q->param('blog_id');
+    my $template_id = $q->param('id');
+    my $template    = MT::Template->load($template_id);
     my $linked_file =
         $template->linked_file()
       ? $template->linked_file 
