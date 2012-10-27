5f442d9c037993307244ff537b8daba045efcde9
cool2ikou <cool2ikou@cool2ikou.com>
2012-10-27 23:41:15 +0900

    Plugin.pm changed and Transform.pm bug fixed

--- SetTemplateLink/lib/SetTemplateLink/Plugin.pm
+++ SetTemplateLink/lib/SetTemplateLink/Plugin.pm
@@ -14,11 +14,10 @@ sub hdlr_set_linkedfile_templates {
     return $app->permission_denied() unless $app->user->is_superuser;
 
     my $blog    = $app->blog;
-    my $blog_id = $app->blog->id;
+    my $blog_id = $blog->id;
     my $plugin  = MT->component('SetTemplateLink');
     my $path =
       $plugin->get_config_value( 'set_template_path', "blog:$blog_id" );
-
     return $app->error(
         $app->translate(
             "Should be set the linked path in the plugin setting.: [_1]",
@@ -32,70 +31,66 @@ sub hdlr_set_linkedfile_templates {
     }
 
     if ( $fmgr->exists($path) && $fmgr->can_write($path) ) {
-        my @blogs = MT::Blog->load( { id => $blog_id } );
-        foreach my $sibi (@blogs) {
-            require MT::Template;
-            my @temps = MT::Template->load( { blog_id => $sibi->id } );
-            my $index = 1;
-            my $q     = $app->param;
-            foreach my $temp (@temps) {
+        require MT::Template;
+        my $temp_iter = MT::Template->load_iter( { blog_id => $blog->id } );
+        my $index     = 1;
+        my $q         = $app->param;
+        my @steps;
+        while ( my $temp = $temp_iter->() ) {
+            my $template_id = $temp->id;
+            my $template_name = $temp->name;
+            push(@steps, "Success setting: $template_name");
+            my $basename    = $temp->identifier . '.mtml';
+            my $linked_path = File::Spec->catfile( $path, $basename );
+            $temp->linked_file($linked_path);
+            #$temp->build_type(1);
+            $temp->save
+              or die $app->error(
+                $plugin->translate(
+                    "Can not save template linked_file.: [_1]",
+                    $temp->errstr
+                )
+              );
+            $app->log( { 
+                message => "Success: templateId: $template_id linked path setting " } );
 
-                #	require MT::TemplateMap;
-                #	my @tempids = MT::Template->load( { text => $temp->text });
-                #	$app->log( { message => "Templates :". Dumper(@tempids)});
-                my $template_id = $temp->id;
-                my $basename    = $temp->identifier . '.mtml';
-                my $linked_path = File::Spec->catfile( $path, $basename );
-                $temp->linked_file($linked_path);
-                $app->log( { message => $template_id } );
+            if ( $temp->build_type ) {
+                if ( $temp->type eq 'index' ) {
+                    $q->param( 'type',            'index-' . $temp->id );
+                    $q->param( 'tmpl_id',         $temp->id );
+                    $q->param( 'single_template', 1 );
+                    require MT::CMS::Blog;
+                    MT::CMS::Blog::rebuild_pages($app);
+                }
+                elsif ($temp->type eq 'archive'
+                    || $temp->type eq 'category'
+                    || $temp->type eq 'page'
+                    || $temp->type eq 'individual' )
+                {
+                    my $static_maps = delete $app->{static_dynamic_maps};
+                    require MT::TemplateMap;
+                    my $terms = { blog_id => $blog_id };
 
-                #my $c = $fmgr->get_data($linked_path);
-                #$temp->text($c);
-                $temp->build_type(1);
-                $temp->save
-                  or die $app->error(
-                    $plugin->translate(
-                        "Can not save template linked_file.: [_1]",
-                        $temp->errstr
-                    )
-                  );
-
-                if ( $temp->build_type ) {
-                    if ( $temp->type eq 'index' ) {
-                        $q->param( 'type',            'index-' . $temp->id );
-                        $q->param( 'tmpl_id',         $temp->id );
-                        $q->param( 'single_template', 1 );
-                        require MT::CMS::Blog;
-                        MT::CMS::Blog::rebuild_pages($app);
-                    }
-                    elsif ($temp->type eq 'archive'
-                        || $temp->type eq 'category'
-                        || $temp->type eq 'page'
-                        || $temp->type eq 'individual' )
-                    {
-                        my $static_maps = delete $app->{static_dynamic_maps};
-                        require MT::TemplateMap;
-                        my $terms = { blog_id => $blog_id };
-
-                        if (%$terms) {
-                            my @maps = MT::TemplateMap->load($terms);
-                            my @ats = map { $_->archive_type } @maps;
-                            if ( $#ats >= 0 ) {
-                                $q->param( 'type', join( ',', @ats ) );
-                                $q->param( 'with_indexes',    1 );
-                                $q->param( 'no_static',       1 );
-                                $q->param( 'template_id',     $temp->id );
-                                $q->param( 'single_template', 1 );
-                                require MT::CMS::Blog;
-                                MT::CMS::Blog::start_rebuild_pages($app);
-                            }
-                        }
+                    if (%$terms) {
+                        my @maps = MT::TemplateMap->load($terms);
+                        my @ats = map { $_->archive_type } @maps;
+                        if ( $#ats >= 0 ) {
+                            $q->param( 'type', join( ',', @ats ) );
+                            $q->param( 'with_indexes',    1 );
+                            $q->param( 'no_static',       1 );
+                            $q->param( 'template_id',     $temp->id );
+                            $q->param( 'single_template', 1 );
+                            require MT::CMS::Blog;
+                            MT::CMS::Blog::start_rebuild_pages($app);
+                         }
                     }
                 }
-                $index++;
             }
-            $app->call_return;
         }
+        #my %param;
+        #my $json_steps = MT::Util::to_json(\@steps);
+        #$param{json_steps} = $json_steps;
+        $app->call_return;
     }
     else {
         return $app->error(
--- SetTemplateLink/lib/SetTemplateLink/Transformer.pm
+++ SetTemplateLink/lib/SetTemplateLink/Transformer.pm
@@ -7,19 +7,17 @@ use Data::Dumper;
 sub hdlr_template_source_edit_template {
     my ($cb, $app, $tmpl_ref) = @_;
     my $jquery_old = <<EOF; 
-    jQuery('button.save, button.publish').click(function() {
+    jQuery(window).bind('pre_autosave', function(){
         syncEditor();
-        jQuery('form#template-listing-form > input[name=__mode]').val('save');
     });
 EOF
     $jquery_old = quotemeta($jquery_old);
     $jquery_old =~ s!(\\ )+!\\s+!g;
     my $jquery_new = <<EOF;
-    jQuery('button.save, button.publish').click(function() {
+    jQuery(window).bind('pre_autosave', function(){
         syncEditor();
-        jQuery('form#template-listing-form > input[name=__mode]').val('save');
     });
-    if(jQuery('#title').val()) {
+    if(!jQuery('#title').val().match(/[^\x01-\x7E]/g)) {
         jQuery('input#linked_file').val( jQuery('input#linked_file').val().replace(/([^\/]+?)\$/, jQuery('input#title').val()+ "\.mtml"));
         jQuery('button.save').focus();
     }
@@ -38,15 +36,15 @@ EOF
 
 sub hdlr_template_param_edit_template {
     my ( $cb, $app, $param, $tmpl ) = @_;
-
-    my $blog_id = $app->blog->id;
-    my $plugin  = MT->component('SetTemplateLink');
-    my $config_path =
-      $plugin->get_config_value( 'set_template_path', "blog:$blog_id" );
     my $q           = $app->param;
     my $blog_id     = $q->param('blog_id');
     my $template_id = $q->param('id');
+    my $plugin  = MT->component('SetTemplateLink');
+    my $config_path =
+      $plugin->get_config_value( 'set_template_path', "blog:$blog_id" );
+    $app->log({  message => $config_path });
     my $template    = MT::Template->load($template_id);
+    $app->log({  message => Dumper($template) });
     my $linked_file =
         $template->linked_file()
       ? $template->linked_file 
